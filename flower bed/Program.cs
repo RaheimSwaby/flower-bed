3dff1abddbf146dc806ea6f8bc7bc4c4a1b7588d
Rahei <Rahei@RAHEIMSPC>
2024-12-04 12:04:07 -0500

    Add project files.

---
+++ flower bed/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace flower_bed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] flowerbed1 = { 1, 0, 0, 0,1 };
+            int n1 = 1;
+            Console.WriteLine(Canplaceflower(flowerbed1,n1));
+            int[] flowerbed2 = { 1, 0, 0, 0, 1 };
+            int n2 = 2;
+            Console.WriteLine(Canplaceflower(flowerbed2,n2));
+        }
+
+
+        public static bool Canplaceflower(int[] flowerbed , int n) 
+        {
+            int count = 0;  
+            for (int i = 0; i < flowerbed.Length; i++) 
+            {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i -1 ] == 0) && (i == flowerbed.Length-1 || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1; count++;
+                    if (count >= n) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
